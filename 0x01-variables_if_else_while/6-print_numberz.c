b4a98e9063a926d79618663ba91a52c8e6eb24bf
karura195 <karla.diaz@pucp.pe>
2021-06-18 18:20:25 -0700

    Practicing with if, else and while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints numbers from 0 to 9
+* only using putchar
+* Return: 0
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
