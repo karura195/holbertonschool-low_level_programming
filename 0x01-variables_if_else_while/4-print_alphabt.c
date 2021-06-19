cda227e6aac07a0c0dad7ec619f8197824517e4b
karura195 <karla.diaz@pucp.pe>
2021-06-18 17:58:42 -0700

    Practicing with if, else and while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
 * main - Prints the alphabet in lower case
-*
+* except letters e and q
 * Return: 0
 */
 
 int main(void)
 {
 char ch;
-for(ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' || ch != 'q')
 {
 putchar(ch);
 }
+}
+putchar('\n');
 return (0);
 }
