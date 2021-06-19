d39d491b27700e9b943ee95887197b5facd3e274
karura195 <karla.diaz@pucp.pe>
2021-06-18 17:47:29 -0700

    Practicing with if, else and while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lower and upper case
+*
+* Return: 0
+*/
+
+int main(void)
+{
+char ch;
+char ca;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ca = 'A' ; ca <= 'Z' ; ca++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
