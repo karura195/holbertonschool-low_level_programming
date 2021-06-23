110bb726905785ae7dca2cc677f004209e5f9fac
karura195 <karla.diaz@pucp.pe>
2021-06-22 21:34:10 -0700

    Using headers and nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,28 @@
 
 void jack_bauer(void)
 {
-for (int i = 48; i < 51; i++)
-    {
-        for (int j = 48; j < 58; j++)
-        {
-            for (int a = 48; a < 54; a++)
-            {
-                for (int b = 48; b < 58; b++)
-                {
-                    _putchar(i);
-                    _putchar(j);
-                    _putchar(':');
-                    _putchar(a);
-                    _putchar(b);
-                    _putchar('\n');
-                }
-            }
-         if (i == 50 && j == 51)
-         {
-             break;
-         }
-           
-        }
-    }	
+int i, j, a, b;
+
+for (i = 48; i < 51; i++)
+{
+	for (j = 48; j < 58; j++)
+	{
+		for (a = 48; a < 54; a++)
+		{
+			for (b = 48; b < 58; b++)
+			{
+				_putchar(i);
+				_putchar(j);
+				_putchar(':');
+				_putchar(a);
+				_putchar(b);
+				_putchar('\n');
+			}
+		}
+		if (i == 50 && j == 51)
+		{
+			break;
+		}
+	}
+}
 }
