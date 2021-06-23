f4a60acddd5e82ddae45336c7e84505a0882b3a5
karura195 <karla.diaz@pucp.pe>
2021-06-23 08:16:41 -0700

    Using headers and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - a + b
+ * @a: First integer
+ * @b: Second integer
+ * Return: c = a + b
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+
+	return (c);
+}
