92a4348773435207418181f1ab4cffaf895bdc88
karura195 <karla.diaz@pucp.pe>
2021-06-22 19:47:41 -0700

    Using headers and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: integer
+ * Return: n
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
