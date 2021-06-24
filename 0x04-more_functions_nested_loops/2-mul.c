bedeb874791bf7a8aec080200b8f30fcd62cfde5
karura195 <karla.diaz@pucp.pe>
2021-06-24 16:40:23 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
