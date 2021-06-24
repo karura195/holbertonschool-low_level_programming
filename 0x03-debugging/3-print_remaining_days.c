4b8c035e194588ab894cbb1be7a9f1ab12875081
karura195 <karla.diaz@pucp.pe>
2021-06-24 15:50:11 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,34 +11,30 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-int leap_day = 366 - day;
-int rem_day = 365 - day;
-int d = day - 31;
-
 	if (year % 4 == 0)
 	{
 		if (month > 2 && day > 60)
 		{
 		day++;
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", leap_day);
+		printf("Remaining days: %d\n", 366 - day);
 		}
 		else
 		{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", leap_ day);
+		printf("Remaining days: %d\n", 366 - day);
 		}
 	}
 	else
 	{
 		if (month == 2 && day == 60)
 		{
-		printf("Invalid date: %02d/%02d/%04d\n", month, d, year);
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", rem_day);
+		printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
