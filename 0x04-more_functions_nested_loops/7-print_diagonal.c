1f6066b38cf9213e2261c90d893beb4b217561fe
karura195 <karla.diaz@pucp.pe>
2021-06-24 19:10:23 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line of n steps
+ * @n: number of times \ is printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (j == i)
+			{
+				putchar(92);
+				putchar('\n');
+			}
+			else
+			{
+				putchar('.');
+			}
+		}
+	}
+}
