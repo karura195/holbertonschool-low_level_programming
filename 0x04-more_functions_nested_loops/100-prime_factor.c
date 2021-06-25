6b88115e17ce6ea95329448d4099d41102d2d401
karura195 <karla.diaz@pucp.pe>
2021-06-25 09:52:54 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - prints the largest prime factor of 612852475143
+ * Return: 0
+ */
+
+int main(void)
+{
+i	long int a = 612852475143;
+	int i;
+
+	for (i = 2; i < a; i++)
+	{
+		while (a % i == 0)
+		{
+			a = a / i;
+		}
+	}
+	printf("%d\n", i);
+}
