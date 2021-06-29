e19dd7021853a38931213fb2e3b2d818ce198d2a
karura195 <karla.diaz@pucp.pe>
2021-06-28 22:01:51 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j, k;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < (i / 2); j++)
+	{
+		k = 2 * j;
+		_putchar(*(str + k));
+	}
+	_putchar('\n');
+}
