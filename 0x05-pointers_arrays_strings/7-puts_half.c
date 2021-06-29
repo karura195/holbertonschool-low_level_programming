8356cd805abfa106db454a7e480f1fcce84e2764
karura195 <karla.diaz@pucp.pe>
2021-06-28 22:16:14 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints second half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 == 0)
+	{
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	else
+	{
+		for (j = (i - 1) / 2; j < i; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	putchar('\n');
+}
