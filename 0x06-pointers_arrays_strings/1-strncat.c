3e5e5d8ebaa8509bebb6ddec425949eef4d9970e
karura195 <karla.diaz@pucp.pe>
2021-07-01 21:49:50 -0700

    More pointers and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,24 +12,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
-	int a;
-	int b = 0;
+	int j;
 
 	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
 
-	while (*(src + j) != '\0')
+	for (j = 0 ; (*(src + j) != '\0') && (j < n) ; j++)
 	{
-		j++;
+		*(dest + (i + j)) = *(src + j);
 	}
 
-	for (a = i; a < (i + n); a++)
-	{
-		*(dest + a) = *(src + b);
-		b++;
-	}
 return (dest);
 }
