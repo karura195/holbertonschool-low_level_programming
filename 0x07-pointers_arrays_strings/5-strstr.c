ed7bd9da209a0dcff0ea5b9eebe17122b8803813
karura195 <karla.diaz@pucp.pe>
2021-07-06 10:35:00 -0700

    Double pointers and arrays

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring
+ * needle in the string haystack
+ * @haystack: string to analyze
+ * @needle: string to find
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+	}
+	return (0);
+}
