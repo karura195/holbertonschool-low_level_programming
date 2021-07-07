ded953afc589466e020a1cacdfc9b7851b468f5e
karura195 <karla.diaz@pucp.pe>
2021-07-06 21:29:30 -0700

    Double pointers and arrays

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: value to change
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
