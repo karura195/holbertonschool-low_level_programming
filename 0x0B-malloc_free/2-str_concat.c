864d8911e170dd467f648ef6bb3cd47460bd95a5
karura195 <karla.diaz@pucp.pe>
2021-07-13 18:41:59 -0700

    Working with malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - The returned pointer should point to
+ * a newly allocated space in memory which contains the contents of s1,
+ * followed by the contents of s2, and null terminated
+ * @s1: first string
+ * @s2: second string, goes after s1
+ * Return: array
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *array;
+	int i = 0;
+	int j = 0;
+	int k, l;
+
+	while (*(s1 + i) != '\0')
+	{
+		i++;
+	}
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+	}
+
+	array = malloc((i + j) * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (k = 0; k < i; k++)
+		{
+			*(array + k) = *(s1 + k);
+		}
+		for (l = 0; l < j; l++)
+		{
+			*(array + k + l) = *(s2 + l);
+		}
+	}
+	return (array);
+}
