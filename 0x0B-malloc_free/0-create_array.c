0d6e295c4e7de4444494e41261611774d2f5a727
karura195 <karla.diaz@pucp.pe>
2021-07-13 18:16:31 -0700

    Working with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,13 +20,17 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(size * sizeof(char));
 
-	if (size != 0)
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	else if (size != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			*(array + i) = c;
 		}
 	}
-	free(array);
+
 	return (array);
 }
