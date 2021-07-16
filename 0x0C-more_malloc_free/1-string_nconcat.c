f33b1d7b6e148629d28f02b8e3ca4f836ad04a21
karura195 <karla.diaz@pucp.pe>
2021-07-15 17:53:39 -0700

    Malloc, calloc and realloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 	}
 	str = malloc((i + n) * sizeof(char));
-	if ((str == NULL) || (s1 == NULL && s2 == NULL))
+	if (str == NULL)
 	{
 		return (NULL);
 	}
