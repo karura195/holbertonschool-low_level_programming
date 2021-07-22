64e8624398e363602e3ec9144b0d594d0c5698a9
karura195 <karla.diaz@pucp.pe>
2021-07-21 21:57:22 -0700

    Pointers to functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,14 +18,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
-		{
+		if (strcmp(s, ops[i].op) == 0)
 			break;
-		}
+		i++;
 	}
 	return (ops[i].f);
 }
