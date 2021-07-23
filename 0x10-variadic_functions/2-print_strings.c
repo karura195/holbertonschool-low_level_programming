daf53773591753acd95cefc1742dc8db2761705b
karura195 <karla.diaz@pucp.pe>
2021-07-22 21:49:59 -0700

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
-		if (va_arg(ap, char *))
+		if (va_arg(ap, char *) != NULL)
 		{
 			printf("%s", va_arg(ap, char *));
 		}
