e20bbb794c3fcd5be3cd08ef02013f384565d2cc
karura195 <karla.diaz@pucp.pe>
2021-08-04 21:18:46 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+	head = NULL;
+}
