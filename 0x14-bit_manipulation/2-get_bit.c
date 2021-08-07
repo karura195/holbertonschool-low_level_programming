56004adc9e6f89601228502c772483b11e112b89
karura195 <karla.diaz@pucp.pe>
2021-08-06 17:39:37 -0700

    Gets the bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: decimal number
+ * @index: the index, starting from 0 of the bit you want to get
+ *
+ * Return: the value of the bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n > 1)
+	{
+		for (i = 0; i < index; i++)
+		{
+			n = n >> 1;
+		}
+	}
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
