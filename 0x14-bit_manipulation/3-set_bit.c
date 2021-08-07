6fa6bc9f3754b2596f6448861cf694f1fd74d317
karura195 <karla.diaz@pucp.pe>
2021-08-06 21:30:37 -0700

    Sets the bit to 1 at a given index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: decimal number
+ * @index: the index, starting from 0 of the bit you want to set
+ *
+ * Return: n + the bit
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	if (*n >= 0)
+	{
+		*n = *n + _pow(2, index);
+	}
+	return (*n);
+}
