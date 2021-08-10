be222c9d4ec4fc7f5a557600d79131af27e9ca2b
karura195 <karla.diaz@pucp.pe>
2021-08-09 21:51:03 -0700

    Read and print a textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/** read_textfile - reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: length of the file
+ *
+ * Return: 0 if fails, the length if succeeds
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, fdr, fdw;
+	char *buf;
+
+	if ( filename == NULL)
+	{
+		return (0);
+	}
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	fdr = read(fd, buf, letters);
+	fdw = write(STDOUT_FILENO, buf, fdr);
+
+	if (fd == -1 || fdr == -1 || fdw == -1 || fdr != fdw)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (fdw);
+}
