90194be4335f823273725b24193a07ebeca3c550
karura195 <karla.diaz@pucp.pe>
2021-08-09 21:55:49 -0700

    Read and print a textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** read_textfile - reads a text file and prints it
+/**
+ * read_textfile - reads a text file and prints it
  * to the POSIX standard output
  * @filename: name of the file to read
  * @letters: length of the file
@@ -13,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, fdr, fdw;
 	char *buf;
 
-	if ( filename == NULL)
+	if (filename == NULL)
 	{
 		return (0);
 	}
