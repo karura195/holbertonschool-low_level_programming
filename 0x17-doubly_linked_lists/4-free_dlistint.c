c73b8bb06b877111c4d754b2d2b920e80c39f00a
karura195 <karla.diaz@pucp.pe>
2021-09-16 19:32:43 -0700

    Frees a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a list
+ * @head: head of list
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *h;
+
+	while (head != NULL)
+	{
+		new = head->next;
+		free(head);
+		head = new;
+	}
+}
